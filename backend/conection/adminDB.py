f0ead5909bd479e69ec3a606a44742ed4652c235
morquecho-z <mssm011009@gmail.com>
2023-07-18 17:46:36 -0700

    add: cree el modelo para la base de datos

--- backend/conection/adminDB.py
+++ backend/conection/adminDB.py
@@ -1,21 +1,21 @@
 from backend.bdst.conexion import Database,INCREMENT,KEY,NOT_NULL
 
 class AdminDatabase(Database):
-    def __init__(self, _user: str, _password: str, _host: str,nameDB:str="STR-16",nameTable:str = 'TSS') -> None:
+    def __init__(self, _user: str, _password: str, _host: str,nameDB:str="str-16",nameTable:str = 'tss') -> None:
         super().__init__(_user, _password, _host)
         self.__name_db =  nameDB
-        self.__name_table = nameTable
-        self.EMAIL_PROPERTY = "000"
-        self.PASSWORD_PROPERTY = "111"
-        self.GROUP_PROPERTY = "222"
+        self.tableName = nameTable
+        self.EMAIL_PROPERTY = "mail"
+        self.PASSWORD_PROPERTY = "pass"
+        self.GROUP_PROPERTY = "group"
 
         db_ok = self.__init_database()
         if not db_ok:
             raise Exception("Could not initialize database")
         tb_ok = self.__init_table()
         
-        if not tb_ok:
-            raise Exception("Could not initialize table")
+        if not tb_ok == True:
+            raise Exception(f"Could not initialize table:")
         
     def __init_database(self):
         try:
@@ -28,24 +28,28 @@ class AdminDatabase(Database):
     def __init_table(self):
         try:
             self.createTable(
-                self.__name_table,
+                self.tableName,
                 (("id",int,f"{KEY} {INCREMENT}"),
                 (self.EMAIL_PROPERTY,str,""),
                  (self.PASSWORD_PROPERTY,str,""),
                  (self.GROUP_PROPERTY,str,""),),
                  50
             )
+            self.setTable(self.tableName)
             return True
-        except:
+        except Exception as e:
+            print(e)
             return False
         
 
-    def add_frame(self,email:str,password:str)->None|Exception:
+    def add_frame(self,email:str,password:str,group:str)->None|Exception:
         """
         Add a new frame in the database...
         """
         try:
-            result = self.insertV(email,password)
+            self.setTable(self.tableName)
+            columns = (self.EMAIL_PROPERTY,self.PASSWORD_PROPERTY,self.GROUP_PROPERTY)
+            result = self.insertVForC(columns,email,password,group)
             if not result == True:
                 raise Exception("No se pudo hacer la inserción del Frame en la base de datos.")
             
@@ -53,10 +57,13 @@ class AdminDatabase(Database):
             return e
 
     def get_frames(self)->list:
+        self.setTable(self.tableName)
         return self.selectC((self.EMAIL_PROPERTY,self.PASSWORD_PROPERTY,self.GROUP_PROPERTY))
     
-    def remove_frame(self,email:str,password:str,group:str)->bool:
-        return self.delete({self.EMAIL_PROPERTY:email,self.PASSWORD_PROPERTY:password,self.GROUP_PROPERTY:group})
+    def delete_frame(self,email:str,password:str,group:str)->bool:
+        self.setTable(self.tableName)
+        delete = {self.EMAIL_PROPERTY:email,self.PASSWORD_PROPERTY:password,self.GROUP_PROPERTY:group}
+        return self.delete(delete)
     
 torres = AdminDatabase(
     "root",
