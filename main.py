d4483f59c9d1a13450492f38dc8a61b216e8dcc3
morquecho-z <mssm011009@gmail.com>
2023-07-20 10:01:48 -0700

    update: Agregue la funcionalidad de ocultar la pantalla de los Frames hasta poner la 'palabra magica'

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from ui.post_window import *
-from PySide6.QtCore import QRect
 
-app = QApplication()
-resolution = app.primaryScreen().geometry().getRect()
-width,height = [resolution[-2],resolution[-1]]
-window = MainWindow()
-window.setGeometry(width//4,height//4,width//2,height//2)
-window.show()
-app.exec()
+if __name__ == '__main__':
+    app = QApplication()
+    resolution = app.primaryScreen().geometry().getRect()
+    width,height = [resolution[-2],resolution[-1]]
+    window = MainWindow()
+    window.setGeometry(width//4,height//4,width//2,height//2)
+    window.show()
+    app.exec()
